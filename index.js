8d5540c94c9e3c1e1a1cab859dded979ab6b5587
Parth <partharora99160808@gmail.com>
2020-11-07 05:27:57 +0530

    added my content location

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'mustache');
 app.engine('mustache', mustacheExpress());
 
-const CONTENT = '/run/media/user/DATA/tv series';
+// const CONTENT = '/run/media/user/DATA/tv series';
+const CONTENT = '/media/parth/8050EED950EED4C6/Movies';
 
 app.use('/', serveIndex(CONTENT, {icons: true}));
 
@@ -24,7 +25,7 @@ app.use((req, res, next) => {
     let fileName = '/' + req.url;
     let videoName = path.basename(fileName);
     let videoSource = fileName;
-    let subtitleSource = fileName.replace('.mp4', '.vtt');
+    let subtitleSource = fileName.replace(/\.(mp4|mkv)/, '.vtt');
     res.render('displayVideo.mustache', {
         videoName: videoName,
         videoSource: fileName,

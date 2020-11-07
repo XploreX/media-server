74b231a840499182c0aa8b5ff2b1d5a2d6ae45a9
manorit chawdhry <manorit2001@gmail.com>
2020-11-07 19:03:57 +0530

    optimized the code a bit
    - added 404 response
    - added options for handling more mimetypes of videos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const serveIndex = require('serve-index');
 const url = require('url');
 const path = require('path');
@@ -14,28 +14,46 @@ app.engine('mustache', mustacheExpress());
 
 const CONTENT = '/run/media/user/DATA/tv series';
 
-app.use('/', serveIndex(CONTENT, {icons: true,
-    filter:function(file,pos,list,dir) {
-			console.log(arguments);
-			return ((fs.existsSync(path.join(dir,file)) && fs.lstatSync(path.join(dir,file)).isDirectory()) || file.indexOf('.mp4') >= 1);
-		}
+app.use('/', serveIndex(CONTENT, {
+    icons: true,
+    filter: function (file, pos, list, dir) {
+        // console.log(arguments);
+        return ((fs.existsSync(path.join(dir, file)) && fs.lstatSync(path.join(dir, file)).isDirectory()) || file.indexOf('.mp4') >= 1);
+    }
 }));
 
 app.use('/public', express.static(CONTENT));
 
 app.use((req, res, next) => {
     req.url = decodeURI(req.url);
-    req.url = path.join('public', req.url);
-    console.log(req.url);
-    let fileName = '/' + req.url;
-    let videoName = path.basename(fileName);
-    let videoSource = fileName;
-    let subtitleSource = fileName.replace('.mp4', '.vtt');
-    res.render('displayVideo.mustache', {
-        videoName: videoName,
-        videoSource: fileName,
-        subtitleSource: subtitleSource
-    });
+    let absoluteFilePath = path.join(CONTENT, unescape(req.url))
+    console.log(absoluteFilePath)
+    // added a 404 when the url is invalid
+    if (!fs.existsSync(absoluteFilePath)) {
+        res.status(404)
+        res.end("Not found")
+    }
+    else {
+        req.url = path.join('public', req.url);
+        console.log(req.url);
+        let fileName = '/' + req.url;
+        let videoName = path.basename(fileName);
+        let videoSource = fileName;
+        let subtitleSource = ''
+        let videoType = ''
+        // we can add more mime types this way
+        if (fileName.indexOf(".mp4")) {
+            videoType = "video/mp4"
+            subtitleSource = fileName.replace('.mp4', '.vtt');
+        }
+        res.render('displayVideo.mustache', {
+            videoName: videoName,
+            videoSource: fileName,
+            videoType: videoType,
+            subtitleSource: subtitleSource
+        });
+    }
+
 })
 
 module.exports = app;

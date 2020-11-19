9ffb17398b7b48e8430159237baf5ade25ce22a7
Parth <partharora99160808@gmail.com>
2020-11-19 13:25:31 +0530

    fixed linting

--- src/routes/api/info.js
+++ src/routes/api/info.js
@@ -4,10 +4,9 @@ const {StatusCodes} = require('http-status-codes');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.post('/keycodes', (req, res, next)=>{
+router.post('/keycodes', (req, res, next) => {
   console.log(req.body);
   res.sendStatus(StatusCodes.OK);
 });
-
 
 module.exports = router;

c8578fa55bc693370437cb5950a691104fca896b
manorit chawdhry <manorit2001@gmail.com>
2020-12-05 00:56:29 +0530

    added next and prev routers

--- src/user/routes/content.js
+++ src/user/routes/content.js
@@ -19,7 +19,6 @@ router.get(
     serveIndex(content, {
       icons: true,
       filter: function(file, pos, list, dir) {
-      // console.log(arguments);
         return (
           (fs.existsSync(path.join(dir, file)) &&
           fs.lstatSync(path.join(dir, file)).isDirectory()) ||
@@ -37,6 +36,7 @@ router.get('/*', (req, res, next) => {
   if (!services.video.isSupportedVideo(absoluteFilePath)) {
     next();
   } else {
+    const originalFileName = path.basename(absoluteFilePath);
     req.url = path.join('public', req.url);
     console.log(req.url);
     const fileName = '/' + req.url;
@@ -52,15 +52,25 @@ router.get('/*', (req, res, next) => {
     ) {
       subtitleSource = fileName.replace(supportedVideoFormatsReg, '.vtt');
     }
-    // console.log(videoSource, subtitleSource);
-    {
-      res.render('displayVideo.mustache', {
-        videoName: videoName,
-        videoSource: videoSource,
-        videoType: videoType,
-        subtitleSource: subtitleSource,
-      });
+    const dir = path.dirname(absoluteFilePath);
+    const files = fs.readdirSync(dir).filter((file) => {
+      return supportedVideoFormatsReg.test(file);
+    });
+    const pos = files.indexOf(originalFileName);
+    let next = '#';
+    let prev = '#';
+    if (pos + 1 < files.length) {
+      next = files[pos + 1];
     }
+    if (pos - 1 >= 0) prev = files[pos - 1];
+    res.render('displayVideo.mustache', {
+      videoName: videoName,
+      videoSource: videoSource,
+      videoType: videoType,
+      subtitleSource: subtitleSource,
+      next: next,
+      prev: prev,
+    });
   }
 });
 
